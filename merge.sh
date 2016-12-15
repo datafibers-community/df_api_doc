d7decf5397788a8fbe8744ebeabfe7be89704221
Will <willddy@gmail.com>
2016-12-15 15:27:51 +0000

    update

--- merge.sh
+++ merge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "This is to copy the apidoc to the DF Data Service Code"
-rm -rf ../df_data_service/src/main/resource/apidoc
-cp -r apidoc ../df_data_service/src/main/resource/
+rm -rf ../df_data_service/src/main/resources/apidoc
+cp -r apidoc ../df_data_service/src/main/resources/
 
 echo "API document is merged."
