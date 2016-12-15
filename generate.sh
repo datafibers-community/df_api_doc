531e47d72efd331ce14f8749b3ec88c729aa8dca
Will <willddy@gmail.com>
2016-12-15 15:24:28 +0000

    update

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-cp ../df_data_service/src/main/java/com/datafibers/service/ stage
+cp ../df_data_service/src/main/java/com/datafibers/service/DFDataProcessor.java stage
 apidoc -f ".*\\.java$" -i stage/ -o apidoc/
 
 for file in stage/*.java; do
--- merge.sh
+++ merge.sh
@@ -2,7 +2,6 @@
 set -e
 echo "This is to copy the apidoc to the DF Data Service Code"
 rm -rf ../df_data_service/src/main/resource/apidoc
-cp apidoc ../df_data_service/src/main/resource/
-apidoc -f ".*\\.java$" -i stage/ -o apidoc/
+cp -r apidoc ../df_data_service/src/main/resource/
 
 echo "API document is merged."
